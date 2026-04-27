33a94aea65eccf7d5c9e458ed756e18083aa8786
nombreUsuario <nombreEmail>
2026-04-27 10:29:12 -0300

    Mostrar Peleador

--- src/main/java/com/mz/PokeApi/MzApplication.java
+++ src/main/java/com/mz/PokeApi/MzApplication.java
@@ -3,12 +3,17 @@ package com.mz.PokeApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.mz.PokeApi.entidades.Peleador;
+import com.mz.PokeApi.entidades.Arma;
 
 @SpringBootApplication
 public class MzApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MzApplication.class, args);
+		Peleador peleador1 = new Peleador(1L, "Pikachu", 100, 50, 20.0f, null, null, null);
+		peleador1.imprimir();
 	}
+
 
 }
