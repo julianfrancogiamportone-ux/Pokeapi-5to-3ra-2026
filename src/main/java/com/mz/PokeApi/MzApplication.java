ed19def8d9c1b05a5cce2988389cbe1bade8365c
GeroPerez <geromean23@gmail.com>
2026-04-27 10:39:47 -0300

    mostrar ataque

--- src/main/java/com/mz/PokeApi/MzApplication.java
+++ src/main/java/com/mz/PokeApi/MzApplication.java
@@ -4,11 +4,17 @@ package com.mz.PokeApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mz.PokeApi.entidades.Ataque;
+
 @SpringBootApplication
 public class MzApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MzApplication.class, args);
+		Ataque lanzallama = new Ataque(1L, "lanzallama", 15, 5000);
+		lanzallama.imprimir();
+
 	}
 
+	
 }
