982b7899f696a61a52a38c1c730c42e4d45b75cb
GeroPerez <geromean23@gmail.com>
2026-06-01 10:50:12 -0300

    Service, Repository, controller de ataque

---
+++ src/main/java/com/mz/PokeApi/Repository/AtaqueRepository.java
@@ -0,0 +1,10 @@
+package com.mz.PokeApi.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository; 
+import com.mz.PokeApi.entidades.Ataque;
+
+ 
+public interface AtaqueRepository extends JpaRepository<Ataque, Long> { 
+} 
+
+
